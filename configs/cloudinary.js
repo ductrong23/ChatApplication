d6ba9440ceba5add48ecd102cb6ca747677d0649
ductrong23 <134128692+ductrong23@users.noreply.github.com>
2026-04-28 16:20:56 +0700

    Update cloudinary.js

--- configs/cloudinary.js
+++ configs/cloudinary.js
@@ -1,9 +1,21 @@
+// const cloudinary = require("cloudinary").v2;
+
+// cloudinary.config({
+//   cloud_name: "djqe3fdeq",
+//   api_key: "328313576248613",
+//   api_secret: "eQ3eps1aagYVc44R0TDbKQkngXM",
+//   secure: true,
+// });
+
+// module.exports = cloudinary;
+
+
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-  cloud_name: "djqe3fdeq",
-  api_key: "328313576248613",
-  api_secret: "eQ3eps1aagYVc44R0TDbKQkngXM",
+  cloud_name: process.env.CLOUDINARY_NAME, // Phải là biến môi trường
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
   secure: true,
 });
 
